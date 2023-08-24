ab3f8c6c636eff6b706c544cef93572e7a21ef72
anshuman-nivas <125132858+anshuman-nivas@users.noreply.github.com>
2023-08-25 01:24:33 +0530

    adding many components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import Navbar from './components/navbar'
-import './App.css'
-import Middlesection from './components/middlesection'
-import IMG from './bg.webp'  
+import React from 'react';
+import Navbar from './components/navbar';
+import './App.css';
+import Mid from './components/middlesection';
+import Carousel from './components/carousel';
+
 function App() {
   return (
     <div>
-      <img className='img' src={IMG}   alt='bg'/>
-      <Navbar/>
-      <Middlesection/>
+      <div className='bg'>
+        <button id='btn1' className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+        <Carousel />
+      </div>
+      <div className="hehe">
+        <Navbar />
+        <Mid />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/middlesection.js
+++ src/components/middlesection.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './middlesection.css'
 
 
-function App() {
+function Mid() {
   return (
     <div>
-     
+
       <div className="container">
         <div className="left-box">
           <h1 className="heading1"><strong><b><i>SPARDHA 2023<br />NEVER GIVE UP</i></b></strong></h1>
@@ -16,18 +16,13 @@ function App() {
               <button type="button" className="btn btn-danger btn-lg">REGISTER</button>
             </div>
             <div className="button2">
-              <button type="button"className="btn btn-outline-light btn-lg">MATCHES</button>
+              <button type="button" className="btn btn-outline-light btn-lg">MATCHES</button>
             </div>
           </div>
         </div>
         <div className="right-box">
-          <div className="card-wrapper">
-            <div className="card">
-              <div className="card-body">
-                <div><h1 className="days"><strong><i>12</i></strong></h1></div>
-                <p className="card-text">Days Remaining</p>
-              </div>
-            </div>
+          <div className='card'>
+            <div className='ctext'></div>
           </div>
         </div>
       </div>
@@ -35,4 +30,4 @@ function App() {
   )
 }
 
-export default App
+export default Mid;
