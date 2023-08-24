f75877eb9badfb4ce6ad127dc921e9597ebfce1a
apoorvdubey23 <apoorvd23@gmail.com>
2023-08-24 18:20:56 +0530

    added navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './navbar.css'
+function navbar() {
+  return (
+    <div className='navbar'>
+        <button className='nvbr'>LOGO</button>
+        <div className='empty1'></div>
+        <button className='nvbr'>HOME</button>
+        <button className='nvbr'>ABOUT</button>
+        <button className='nvbr'>MATCHES</button>
+        <button className='nvbr'>TEAMS</button>
+        <button className='nvbr'>CONTACT</button>
+        <button className='nvbr'>GALLERY</button>
+        <div className='empty2'></div>
+        <div className='socialmedialinks'>
+            <div className='smlink'><i className="fa-brands fa-facebook fa-xl"/></div>
+            <div className='smlink'><i className="fa-brands fa-instagram fa-xl"></i></div>
+            <div className='smlink'><i className="fa-brands fa-twitter fa-xl"></i></div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default navbar
