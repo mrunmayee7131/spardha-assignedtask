24d933f12201308219c97c141b4bf76c08c1e5e7
mrunmayee7131 <139473355+mrunmayee7131@users.noreply.github.com>
2023-08-26 12:30:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
     <div>
       <section className='sctn'>
         <div className='bg'>
-          <button id='btn1' className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          {/* <button id='btn1' className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Previous</span>
           </button>
           <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Next</span>
-          </button>
+          </button> */}
           <Carousel />
         </div>
         <div className="hehe">
